386bed085c0aa7d713a2f94f70ceaef483cc689b
Matthew Dahl <dahl.matthew@gmail.com>
2016-09-15 10:11:30 -0700

    Add tests

--- tests/integration/components/react-component-test.js
+++ tests/integration/components/react-component-test.js
@@ -1,6 +1,8 @@
 import {expect} from 'chai'
 import {describeComponent, it} from 'ember-mocha'
 import hbs from 'htmlbars-inline-precompile'
+import {beforeEach, describe} from 'mocha'
+import sinon from 'sinon'
 
 describeComponent(
   'react-component',
@@ -9,9 +11,63 @@ describeComponent(
     integration: true
   },
   function () {
+    let props, sandbox
+
+    beforeEach(function () {
+      sandbox = sinon.sandbox.create()
+
+      props = {
+        componentDidMount: sandbox.spy(),
+        componentWillMount: sandbox.spy(),
+        componentWillUnmount: sandbox.spy(),
+        renderComponent: true
+      }
+
+      this.setProperties(props)
+
+      this.render(hbs`
+        {{#if renderComponent}}
+          {{react-component
+            componentDidMount=componentDidMount
+            componentWillMount=componentWillMount
+            componentWillUnmount=componentWillUnmount
+          }}
+        {{/if}}
+      `)
+    })
+
     it('renders', function () {
-      this.render(hbs`{{react-component}}`)
       expect(this.$()).to.have.length(1)
+    })
+
+    it('calls componentWillMount() once', function () {
+      expect(props.componentWillMount.callCount).to.equal(1)
+    })
+
+    it('calls componentDidMount() once', function () {
+      expect(props.componentDidMount.callCount).to.equal(1)
+    })
+
+    it('does not call componentWillUnmount', function () {
+      expect(props.componentWillUnmount.callCount).to.equal(0)
+    })
+
+    describe('when component destroyed', function () {
+      beforeEach(function () {
+        this.set('renderComponent', false)
+      })
+
+      it('does not call componentWillMount()', function () {
+        expect(props.componentWillMount.callCount).to.equal(1)
+      })
+
+      it('does not call componentDidMount()', function () {
+        expect(props.componentDidMount.callCount).to.equal(1)
+      })
+
+      it('calls componentWillUnmount once', function () {
+        expect(props.componentWillUnmount.callCount).to.equal(1)
+      })
     })
   }
 )
